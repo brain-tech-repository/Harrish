9dc5cdabe7c6da2f21e95a282af65fa0dc057693
Pawan Kushwah <pawankushwahmail@gmail.com>
2025-10-10 21:42:03 +0530

    fixed bugs

--- app/components/contexts/allDropdownListData cc.tsx
+++ app/components/contexts/allDropdownListData cc.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-import DiscountType from '../../(private)/dashboard/settings/customer/discountType/page';
 import {
   companyList,
   countryList,
