c4e8f12f41182f6db81c6e08b2798c92bf5f18ca
pawankushwah <pawankushwhmail@gmail.com>
2026-01-17 19:27:48 +0530

    create way to handle camelcase url

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,32 @@ const nextConfig: NextConfig = {
             },
         ]
     },
+    async rewrites() {
+    return [
+      {
+        // Add your main camelCase folders here
+        source: '/advancepayment/:path*',
+        destination: '/advancePayment/:path*',
+      },
+      {
+        source: '/capscollection/:path*',
+        destination: '/capsCollection/:path*',
+      },
+      {
+        source: '/distributorsdelivery/:path*',
+        destination: '/distributorsDelivery/:path*',
+      },
+      {
+        source: '/salesteamload/:path*',
+        destination: '/salesTeamLoad/:path*',
+      },
+      {
+        source: '/salesteamunload/:path*',
+        destination: '/salesTeamUnload/:path*',
+      },
+      // Continue this pattern for your other main folders...
+    ];
+  },
 };
 
 export default nextConfig;
