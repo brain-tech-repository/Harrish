7aaaaf7fd74ea8eacf46ef03af1f9bbce993ebcc
Sugnesh90 <sugnesh@technobren.com>
2026-01-23 13:25:36 +0530

    fixed in merchandiser

--- app/(private)/(dashboard)/(merchandiser)/campaign/page.tsx
+++ app/(private)/(dashboard)/(merchandiser)/campaign/page.tsx
@@ -233,7 +233,7 @@ export default function CampaignPage() {
                     <button
                       type="button"
                       onClick={() => openImageModal(images, 0)}
-                      className="text-blue-600 font-medium hover:underline"
+                      className="text-blue-600 font-medium hover:underline cursor-pointer"
                     >
                       View Images ({images.length})
                     </button>
